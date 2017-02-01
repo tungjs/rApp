c922606fe33c2a0d0b8db3fafa6bfb2f00eca0ec
Tung <Tungnguyen@vsoft.vn>
2017-02-02 05:01:00 +0700

    update structures

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React, { Component } from 'react';
-import Button from './Button.js'
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-console.log(logo);
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Reacts</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-       
-        </p>
-           <Button/>
-      </div>
-    );
-  }
-}
-
-export default App;
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
